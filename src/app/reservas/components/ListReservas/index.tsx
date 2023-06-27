55af3fc5a1f1d1dbe8907b884deead35f211442e
ndbzika <chakcpps@gmail.com>
2023-06-27 17:36:59 -0300

    view: Melhorando grid reservas

--- src/app/reservas/components/ListReservas/index.tsx
+++ src/app/reservas/components/ListReservas/index.tsx
@@ -4,6 +4,7 @@ import Grid from '@mui/material/Unstable_Grid2/Grid2';
 import { useGetData } from '../../../../hooks/useGetData'
 import { ReservasData } from '../../../../interface/ReservasData';
 import { ReservaCard } from '../ReservaCard';
+import { Container } from '@mui/material';
 
 import styles from './styles.module.scss'
 
@@ -14,36 +15,37 @@ export const ListReservas = () => {
   });
 
   return (
-    <>
+    <Container className={styles.container}>
       <Grid 
       container 
       spacing={{ xs: 2, md: 3 }}
       columns={{ xs: 4, sm: 8, md: 12 }}
-      className={styles.container}
-      >
-        <>       
-          {data ?
+      >            
+        { data ?
           Array.isArray(data) 
           &&
-          data.map((reserva: ReservasData) => (
-            <Grid key={reserva.id}>
-              <ReservaCard
-              id={reserva.id}
-              quarto={reserva.quarto}
-              funcionario={reserva.funcionario}
-              hospede={reserva.hospede}
-              dataInicio={reserva.dataInicio}
-              dataFim={reserva.dataFim}
-              status={reserva.status}
-              />
-            </Grid>
-          ))
+            data.map((reserva: ReservasData) => (
+              <Grid key={reserva.id}>
+                  <ReservaCard
+                  id={reserva.id}
+                  quarto={reserva.quarto}
+                  funcionario={reserva.funcionario}
+                  hospede={reserva.hospede}
+                  dataInicio={reserva.dataInicio}
+                  dataFim={reserva.dataFim}
+                  status={reserva.status}
+                />
+              </Grid>
+            ))
+        :
+          <h1 className={styles.centerDiv}>Falha ao buscar dados, tente novamente mais tarde.</h1>
+        }
 
-          :
-            <h1>NEHUMA RESERVA CADASTRADA</h1>
-          }
-      </>
+        { data?.length === 0 ?
+            <h1 className={styles.centerDiv}>Não há quartos</h1>
+            : 
+            null }
       </Grid>
-    </>
+    </Container>
   )
 }
