2c959bca99cb51350476ab5b3543b88e0159de08
ndbzika <chakcpps@gmail.com>
2023-06-27 13:54:34 -0300

    view: Melhorando navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,30 +1,24 @@
 'use client'
 
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import Link from 'next/link'
 
 import styles from './styles.module.scss'
 
-import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
-
-function HomeIcon(props: SvgIconProps) {
-  return (
-    <SvgIcon {...props}>
-      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-    </SvgIcon>
-  );
-}
-
 export const Navbar = () => {
   return (
-    <Box className={styles.container}>
+    <Box component='header' className={styles.navbar}>
+      <Typography component='h2' variant='h5'>
+        Modo ADMIN
+      </Typography>
 
-      <Link href='/quartos' className={styles.links} >Quartos</Link>
-      <Link href='/hospedes' className={styles.links} >Hospedes</Link>
-      <Link href='/' className={styles.links} > <HomeIcon /> </Link>
-      <Link href='/funcionarios' className={styles.links} >Funcionarios</Link>
-      <Link href='/reservas' className={styles.links} >Reservas</Link>
-
+      <Box className={styles.links} component='nav'>
+        <Link href='/' className={styles.link} >Home</Link>
+        <Link href='/quartos' className={styles.link} >Quartos</Link>
+        <Link href='/hospedes' className={styles.link} >Hospedes</Link>
+        <Link href='/funcionarios' className={styles.link} >Funcionarios</Link>
+        <Link href='/reservas' className={styles.link} >Reservas</Link>
+      </Box>
     </Box>
   )
 }
